a0e266735ff7fea0ced2a3a0d085e45f8cbed020
sanzodown <sanzodown@hotmail.fr>
2014-03-03 01:01:48 +0100

    Ajout du dossier inté

--- app/Controllers/controller.php
+++ app/Controllers/controller.php
@@ -1,24 +1,30 @@
 <?php
-class Controller{
-  
-protected $tpl;
-protected $model;
+class Controller
 
-  public function __construct(){
-    $f3=\Base::instance();
-    if($f3->get('PATTERN')!='/signin'&&$f3->get('PATTERN')!='/send'&&!$f3->get('SESSION')&&$f3->get('PATTERN')!='/sendfb'){
+{
+  protected $tpl;
+  protected $model;
+  public
+
+  function __construct()
+  {
+    $f3 = Base::instance();
+    if ($f3->get('PATTERN') != '/signin' && $f3->get('PATTERN') != '/send' && $f3->get('PATTERN') != '/sendfb' && $f3->get('PATTERN') != '/api' && !$f3->get('SESSION'))
+    {
       $f3->reroute('/signin');
     }
   }
 
-  public function beforeroute(){
-    $model=substr(get_class($this),0,strpos(get_class($this),'_')+1).'model';
-    if(class_exists($model)){
-      $this->model=new $model();
+  public
+
+  function beforeroute()
+  {
+    $model = substr(get_class($this) , 0, strpos(get_class($this) , '_') + 1) . 'model';
+    if (class_exists($model))
+    {
+      $this->model = new $model();
     }
   }
-  
-  
-
 }
+
 ?>
